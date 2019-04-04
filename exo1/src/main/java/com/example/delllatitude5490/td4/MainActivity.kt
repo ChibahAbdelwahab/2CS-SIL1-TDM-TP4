84907f8eb7cfba1cf2a89a446f2f3e049f5f4acc
DELL LATITUDE 5490 <fa_chibah@esi.dz>
2019-04-04 17:49:27 +0100

    Initial Commit

---
+++ exo1/src/main/java/com/example/delllatitude5490/td4/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.delllatitude5490.td4
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
